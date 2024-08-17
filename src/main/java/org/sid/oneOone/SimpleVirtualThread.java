a53453930a62fc5f0d2dda0109f6165ce1e84d8f
Siddharth Kulkarni <ext_skulkarni@falabella.cl>
2024-08-17 12:25:21 +0530

    Virtual Threads configurable

--- src/main/java/org/sid/oneOone/SimpleVirtualThread.java
+++ src/main/java/org/sid/oneOone/SimpleVirtualThread.java
@@ -8,23 +8,24 @@ public class SimpleVirtualThread {
     public static void main(String[] args) throws InterruptedException {
         final int THREAD_COUNT = 100000;
         Thread[] threads = new Thread[THREAD_COUNT];
-        IntStream.range(0, THREAD_COUNT).forEach(i -> threads[i] = spawnThread((x) -> x + i, i, true));
+        IntStream.range(0, THREAD_COUNT).forEach(i -> threads[i] = spawnThread((x) -> x + i, i, false));
         for (Thread t : threads) {
             t.join();
         }
     }
 
     private static Thread spawnThread(Function<Integer, Integer> f, int i, boolean isVirtual) {
-        var t = isVirtual ? Thread.ofVirtual() : Thread.ofPlatform();
-        t.name("Thread-"+ i).unstarted(() -> {
+        var t = isVirtual ? Thread.ofVirtual().name("Virtual Thread-"+i) : Thread.ofPlatform().name("Platform Thread-"+i);
+        var t1 = t.unstarted(() -> {
             try {
                 f.apply((int) (Math.random() * 100));
-                System.out.println("Running thread " + Thread.currentThread().getName());
+                Thread.sleep(1);
+                System.out.println("Running " + Thread.currentThread().getName());
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         });
-        t.start();
-        return t;
+        t1.start();
+        return t1;
     }
 }
